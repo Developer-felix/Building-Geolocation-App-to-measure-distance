88b1f5f703cff7c670a84d299b7886a69d3202cf
Developer-felix <onjombafelix1@gmail.com>
2021-03-27 13:21:01 -0700

    Distance Views functionality

--- measurements/views.py
+++ measurements/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_bject_or_404
+from .models import Measurement
 
-# Create your views here.
+def calculate_distance_view(request):
+    obj = get_bject_or_404(Measurement, id=1)
+    context = {
+        'distance' : obj,
+    }
+    return render(request,'measurements/main.html',context)
