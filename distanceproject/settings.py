af7bea90b02e985b118db8e3a9c683d30a5d4e60
Developer-felix <onjombafelix1@gmail.com>
2021-03-27 14:15:56 -0700

    First Lecture complete successfull

--- distanceproject/settings.py
+++ distanceproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'measurements',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
--- measurements/views.py
+++ measurements/views.py
@@ -5,6 +5,12 @@ from .forms import MeasurementModelForm
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.destination = form.cleaned_data.get('destination')
+        instance.location = "Kitale"
+        instance.distance = 1000.00
+        instance.save()
     context = {
         'distance': obj,
         'form' : form,
