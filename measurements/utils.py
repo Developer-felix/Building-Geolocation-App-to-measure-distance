4a4ee02abc51bfff4edf242f6c938a23f1fd11d5
Developer-felix <onjombafelix1@gmail.com>
2021-03-29 10:47:47 -0700

    Getting Ip Address

--- measurements/utils.py
+++ measurements/utils.py
@@ -1,6 +1,14 @@
 from django.contrib.gis.geoip2 import GeoIP2
 #Helper fuction
 
+def get_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def get_geo(ip):
     g = GeoIP2()
     country = g.country(ip)
